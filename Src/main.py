88eb6153a735bd71e2ece82df2b1898264eb4d1c
wolwesking <takacspatrik993@gmail.com>
2022-12-01 18:26:22 +0500

    Final

--- Src/main.py
+++ Src/main.py
@@ -7,8 +7,11 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # Global variables
+usrData = None
+names = None
+domain = None
+password = None
 usrID = None
-
 
 # Initialize window
 window = tk.Tk()
@@ -44,6 +47,9 @@ def main():
     if not database:
         return
 
+    if not usrData:
+        loginPage()
+
     # Creating layout
     frameInput = tk.Frame(window)
     frameInput.pack(side="left")
@@ -57,47 +63,123 @@ def main():
     # Creating input side
     tk.Label(frameInput, text="Domain: ", font=(
         "default", 22)).grid(column=0, row=0)
+
+    global domain
     domain = tk.Entry(frameInput, font=("default", 22))
     domain.grid(column=1, row=0)
 
     tk.Label(frameInput, text="Password: ", font=(
         "default", 22)).grid(column=0, row=1)
+
+    global password
     password = tk.Entry(frameInput, font=("default", 22))
     password.grid(column=1, row=1)
 
-    # TODO Check if logged in
-    SearchButton = tk.Button(frameInput, text="Search", command=searchButton)
+    SearchButton = tk.Button(frameInput, text="Search",
+                             command=lambda: search())
     SearchButton.grid(column=1, row=2)
 
     # Creating output side
 
-    addButton = tk.Button(nav, text="+")
+    addButton = tk.Button(nav, text="+", command=addBt)# TODO command=addBt
     addButton.grid(column=0, row=0)
 
-    deleteButton = tk.Button(nav, text="-")
+    deleteButton = tk.Button(nav, text="-", command=deleteBt)
     deleteButton.grid(column=1, row=0)
-
     newHeight = window.winfo_height() - nav.winfo_height()
+    global names
+    # List functions
     names = tk.Listbox(frameOutput, width=int(
-        window.winfo_width()/2), height=newHeight)
+        window.winfo_width()/2), height=newHeight,)
     names.pack()
-
+    names.bind('<Button-1>', showTextOnClick)
     window.mainloop()
 
-
-def searchButton():
-    if usrID:
-        search()
-        return
+def addBt():
+    global names
+    global domain
+    global password
+    global usrID
+    
+    if len(domain.get()) and len(password.get()):
+        for i in usrData:
+            if domain.get() == i[2]:
+                messagebox.showwarning(message="The domain is already exist")
+                return
+        db = database.cursor()
+        db.execute("INSERT INTO Passwords (UserId, Password, Domain) VALUES (%(id)s, %(pass)s, %(dom)s)", {
+            'id': usrID,
+            'pass': password.get(),
+            'dom': domain.get()
+        })
+        database.commit()
+        messagebox.showinfo(message="Password added")
+        names.insert(0, domain.get())
     else:
-        loginPage()
+        messagebox.showinfo(message="Please fill out both of the forms")
+
+def deleteBt():
+    global names
+
+    id = names.curselection()
+    if len(id):
+        q = messagebox.askyesno(message="Do you want to delete this password?")
+        user = names.get(id, last=None)
+        if q: # TODO TEST
+            db = database.cursor()
+            db.execute("DELETE FROM Passwords WHERE Domain = %(domain)s;", {
+                'domain':user
+            })
+            database.commit()
+            messagebox.showinfo(message="You successfully deleted the password")
+            names.delete(id, id)
+
+
+def search():
+    global names
+    global domain
+    global password
+
+    searchName = domain.get()
+    searchPass = password.get()
+    names.delete(0, tk.END)
+    if searchName:
+        for i in usrData:
+            if searchName == i[2]:
+                names.insert(0, i[2])
+    elif searchPass:
+        for i in usrData:
+            if searchPass == i[1]:
+                names.insert(0, i[2])
+    else:
+        listInit()
+
+
+def showTextOnClick(a):
+    global names
+    global password
+    global domain
+    if len(names.curselection()):
+        for i in usrData:
+            if names.get(names.curselection(), last=None) == i[2]:
+                password.delete(0, tk.END)
+                domain.delete(0, tk.END)
+                password.insert(0, i[1])
+                domain.insert(0, i[2])
+
+
+def listInit():
+    # Update list by user
+    global names
+    if usrData:
+        for i in usrData:
+            names.insert(0, i[2])
 
 
 def loginPage():
     loginPage = tk.Toplevel(window)
     loginPage.geometry("800x600")
     loginPage.title("Login page")
-    
 
     page = tk.Frame(loginPage, width=500)
     page.pack(pady=5)
@@ -146,21 +228,27 @@ def login(usr, psw, window):
             db = database.cursor()
             db.execute(
                 "INSERT INTO Users (Username, Password) VALUES (%(user)s, %(pass)s);", {
-                    'user': usr, 
+                    'user': usr,
                     'pass': psw
-                    })
+                })
             database.commit()
             messagebox.showinfo(message="Registered")
     else:
         # User exist
         global usrID
         usrID = result[0]
+        db = database.cursor()
+        db.execute("SELECT * FROM Passwords WHERE UserID = %(id)s;", {
+            'id': result[0]
+        })
+        user = db.fetchall()
+        global usrData
+        usrData = user
         window.destroy()
+        listInit()
 
 
-# TODO Import everything from the user
 # TODO +, - Buttons
-# TODO Search in database        
 
 
 # Calling Main
--- documentation.md
+++ documentation.md
@@ -5,3 +5,13 @@ Today I created and choosed the tools and created the charts
 
 ## Day 2 29/11/2022
 I managed to get a free sql database provider, also I managed to make the basic layout
+
+## Day 30/11/2022
+I managed to create a login page for my application
+
+## Day 01/12/2022
+I managed to display all of the rows in my database and show it in my text entrys. After this I implemented the search algorythm on both of the rows.
+Also I managed to give functions to the + and - buttons, and with these done, this was my journey of GUI programming. Thanks for reading
+
+## Day 02/12/2022
+I Finished the whole project I only have to compile it to windows/mac/linux
